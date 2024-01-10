cffd2eafd550db151e5ce6c11d548d35d1383625
Tuomas Katajisto <t@ktj.st>
2024-01-10 20:33:43 +0200

    work

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.com/tulema/recipe-software
+
+go 1.19
+
+require github.com/go-chi/chi/v5 v5.0.11
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	workDir, _ := os.Getwd()
+	fileDir := http.Dir(filepath.Join(workDir, "static"))
+	FileServer(r, "/static", fileDir)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
+
+	http.ListenAndServe(":3000", r)
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
+}
